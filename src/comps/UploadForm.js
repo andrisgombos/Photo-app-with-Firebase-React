e6484f7af4604e7aff8a737803881e97f902a8b1
Andras <andrisgombos@gmail.com>
2021-08-13 17:51:40 +0200

    small adjustments to upload form

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UploadForm = () => {
 
+    const [file, setFile] = useState(null)
+    const [error, setError] = useState(null)
+
+    const types = ['image/png', 'image/jpg']
+
+    const uploadHandler = (e) => {
+        let selected = e.target.files[0];
+
+        if (selected !== null && types.includes(selected.type))
+            { setFile(selected)} 
+        else
+            { 
+                setFile(null);
+                setError('Please upload an image file')
+            }
+    }
+
     return (
         <form>
-            <input type='file'/>
+            <input type='file' onChange={uploadHandler}/>
         </form>
     )
 }
