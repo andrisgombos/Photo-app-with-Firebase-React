c80b37b019ea4f7d92575f7311c24cdf7f2e207c
Andras <andrisgombos@gmail.com>
2021-08-15 10:07:52 +0200

    upload form finalized

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -5,13 +5,16 @@ const UploadForm = () => {
     const [file, setFile] = useState(null)
     const [error, setError] = useState(null)
 
-    const types = ['image/png', 'image/jpg']
+    const types = ['image/png', 'image/jpeg']
 
     const uploadHandler = (e) => {
         let selected = e.target.files[0];
+        setFile(selected);
 
         if (selected !== null && types.includes(selected.type))
-            { setFile(selected)} 
+            { setFile(selected)
+              setError('')
+            }
         else
             { 
                 setFile(null);
@@ -22,6 +25,10 @@ const UploadForm = () => {
     return (
         <form>
             <input type='file' onChange={uploadHandler}/>
+            <div className='output'>
+                {error && <div className='error'> {error} </div>}
+                {file && <div> {file.name} </div>}
+            </div>
         </form>
     )
 }
